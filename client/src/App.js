12ab796942b7d188d4f8c741de239d1b6bcb4e40
Forrest <Forrestmillerj@gmail.com>
2020-07-22 16:05:01 -0500

    cleaned up loading todos on compoent render

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Todo from './components/todo';
 import './App.css';
-import { getTodo } from './utils/api';
+import API from './utils/api';
 
 
 
 function App() {
-  // const [todos, setTodos] = useState({})
-  const [todos, setTodos] = useState([
-    {Todo: "todo-1", Completed: true, Time: "2020-07-22T19:13:46.784Z"},
-    {Todo: "todo-2", Completed: false, Time: "2020-07-22T19:13:46.784Z"},
-  ])
-  // useEffect(()=>{
-  //   async function getData() {
-  //     const res = await getTodo();
-  //     setTodos(res.data)
-  //   }
-  //   getData()
-  // },[])
+  const [todos, setTodos] = useState([])
+
+  // load todos when App component renders
+  useEffect( () => {
+    loadTodos()
+  },[])
+
+  // function handling loading todos
+  function loadTodos() {
+    API.getTodos()
+    .then(res => {
+      console.log(res.data);
+      setTodos(res.data);
+    })
+    .catch(err => console.log(err));
+  }
+
   const handleSubmit =  async(e) =>{
     e.preventDefault();
-    // const res = await getTodo();
-    // setTodos(res.data)
     console.log(todos);
   }
+
   return (
     <div className="App">
       <form action="/api/todo" method="POST">
@@ -31,10 +35,10 @@ function App() {
         <input type="submit"></input>
       </form>
       <button onClick={handleSubmit}>click me</button>
-
-      {todos.map(todo =>(
-        <Todo info={todo}/>
-      ))}
+      {/* render todo compoent for each todo loaded. */}
+      { todos.map( todo => (
+        <Todo info={todo} key={todo.Time}/>
+      )) }
     </div>
   );
 }
