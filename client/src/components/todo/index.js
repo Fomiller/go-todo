9122f8c3074304935815895fc380fa1333fe9921
Forrest <Forrestmillerj@gmail.com>
2020-07-22 15:11:24 -0500

    created todo component

---
+++ client/src/components/todo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Todo(props){
+  console.log(props)
+  return(
+    <div>
+      <h2>{props.info.Todo}</h2>
+      {!props.info.Completed ? (
+        <h4>false</h4>
+      ) : (
+        <h4>true</h4>
+      )}
+      <p>{props.info.Time}</p>
+    </div>
+  )
+}
