7ed51ccaa68b792ac65e5e28a306f90c3a229ac5
Forrest <Forrestmillerj@gmail.com>
2020-07-22 14:20:10 -0500

    serving static files, created test routes to connect to client

--- server/main.go
+++ server/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	models "github.com/fomiller/go-todo/server/models/todos"
 )
 
 func main() {
+	// serve static files
+	fs := http.FileServer(http.Dir("../client/"))
+	http.Handle("/", fs)
 	// routes.
-	http.HandleFunc("/", indexhandler)
+	// http.HandleFunc("/", indexhandler)
+	http.HandleFunc("/api/todo/", models.Test)
+	http.HandleFunc("/api/create/", models.CreateTodo)
 
 	// start server.
 	http.ListenAndServe(":8080", nil)
