ee05abbd32c7c27c1f3802e447f53948361d167c
Forrest <Forrestmillerj@gmail.com>
2020-07-22 14:21:13 -0500

    created handler routes for todos

--- server/models/todos/handlers.go
+++ server/models/todos/handlers.go
@@ -1,1 +1,13 @@
 package models
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Test(res http.ResponseWriter, req *http.Request) {
+	// fmt.Println("test")
+	// fmt.Fprintf(res, "working")
+	todos := FindAll()
+	json.NewEncoder(res).Encode(todos)
+}
