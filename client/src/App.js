ed9344b0f4980cee6d1d02235f08a356c070e7e0
Forrest <Forrestmillerj@gmail.com>
2020-07-22 14:18:38 -0500

    added button to console log todos list

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { getTodo } from './utils/api';
+
+
+const handleSubmit =  async(e) =>{
+  e.preventDefault();
+  const res = await getTodo();
+  console.log(res);
+}
 
 function App() {
   return (
     <div className="App">
       <form action="/api/todo" method="POST">
-        <input type="text" name="todo" placeholder="Add a Todo" autoFocus="true"/>
+        <input type="text" name="todo" placeholder="Add a Todo" autoFocus={true}/>
         <input type="submit"></input>
       </form>
+      <button onClick={handleSubmit}>click me</button>
     </div>
   );
 }
