49877a60915558218142de458a67fd2782a3a79f
Forrest <Forrestmillerj@gmail.com>
2020-07-22 12:29:43 -0500

    created findAll function for todos

--- server/models/todos/todos.go
+++ server/models/todos/todos.go
@@ -1,9 +1,77 @@
 package models
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-var todo struct {
-	todo      string
-	completed bool
-	time      time.Time
+	"github.com/fomiller/go-todo/server/config"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Todo struct {
+	Todo      string
+	Completed bool
+	Time      time.Time
+}
+
+var (
+	collection *mongo.Collection
+)
+
+// create todo collection from mongoDB
+func init() {
+	collection = config.CLIENT.Database("go-todo").Collection("todos")
+}
+
+// Return all Todos
+func FindAll() []*Todo {
+	// create slice to documents in
+	var results []*Todo
+
+	// query mongo database
+	cur, err := collection.Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Panic(err)
+	}
+	// close cursor
+	defer cur.Close(context.TODO())
+
+	// iterate through cursor and storing decoded documents inside reults variable
+	for cur.Next(context.TODO()) {
+		var elem Todo
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// store decoded value in results
+		results = append(results, &elem)
+	}
+
+	// if the cursor at any time returns an error log the error
+	if err := cur.Err(); err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("Found multiple documents (array of pointers) : %v\n", results)
+	return results
+}
+
+func FindOne() {
+
+}
+
+func CreateTodo() {
+
+}
+
+func UpdateTodo() {
+
+}
+
+func DeleteTodo() {
+
 }
