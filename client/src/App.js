f5148f5fb7e6148bc91401727c6b13f955984061
Forrest <Forrestmillerj@gmail.com>
2020-07-22 15:10:51 -0500

    app component reenders list of todos.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Todo from './components/todo';
 import './App.css';
 import { getTodo } from './utils/api';
 
 
-const handleSubmit =  async(e) =>{
-  e.preventDefault();
-  const res = await getTodo();
-  console.log(res);
-}
 
 function App() {
+  // const [todos, setTodos] = useState({})
+  const [todos, setTodos] = useState([
+    {Todo: "todo-1", Completed: true, Time: "2020-07-22T19:13:46.784Z"},
+    {Todo: "todo-2", Completed: false, Time: "2020-07-22T19:13:46.784Z"},
+  ])
+  // useEffect(()=>{
+  //   async function getData() {
+  //     const res = await getTodo();
+  //     setTodos(res.data)
+  //   }
+  //   getData()
+  // },[])
+  const handleSubmit =  async(e) =>{
+    e.preventDefault();
+    // const res = await getTodo();
+    // setTodos(res.data)
+    console.log(todos);
+  }
   return (
     <div className="App">
       <form action="/api/todo" method="POST">
@@ -17,6 +31,10 @@ function App() {
         <input type="submit"></input>
       </form>
       <button onClick={handleSubmit}>click me</button>
+
+      {todos.map(todo =>(
+        <Todo info={todo}/>
+      ))}
     </div>
   );
 }
