8ef181c25ca0a198b94645b4c6187c582ab9b6b8
Forrest <Forrestmillerj@gmail.com>
2020-07-23 14:09:59 -0500

    updated todo component to display all info from server/db

--- client/src/components/todo/index.js
+++ client/src/components/todo/index.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Todo(props){
   return(
     <div>
-      <h2>{props.info.Todo}</h2>
-      {!props.info.Completed ? (<h4>false</h4>) : (<h4>true</h4>)}
+      <h2>{props.info.todo}</h2>
+      <h2>{props.info.time}</h2>
+      <h2>{props.info.id}</h2>
+      {!props.info.completed ? (<h4>false</h4>) : (<h4>true</h4>)}
     </div>
   )
 }
