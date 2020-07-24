69b5c6fb7bdc0d4468f2426a783f7ac3f3b52f73
Forrest <Forrestmillerj@gmail.com>
2020-07-24 15:48:22 -0500

    added style sheets for individual components

--- client/src/components/completeBtn/index.js
+++ client/src/components/completeBtn/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './completeBtn.css'
 
 export default function CompleteBtn({props}){
   console.log(props)
   return(
     <div>
-      <button data-todo-id={props.id}>Complete</button>
+      <button data-todo-id={props.id} class="completeBtn">Complete</button>
     </div>
   )
 }
--- client/src/components/deleteBtn/index.js
+++ client/src/components/deleteBtn/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './deleteBtn.css'
 export default function DeleteBtn({props}){
   return(
     <div>
-      <button data-todo-id={props.id}>Delete</button>
+      <button data-todo-id={props.id} class="deleteBtn">Delete</button>
     </div>
   )
 }
