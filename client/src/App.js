c98ab509e8c8bbb07b94411a55c6a791352431ef
Forrest <Forrestmillerj@gmail.com>
2020-07-28 15:50:36 -0500

    removed old code

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,6 @@ import {AppProvider} from './utils/globalContext';
 import Home from './pages/home';
 
 function App() {
-  // establish state
-  // const [todos, setTodos] = useState([])
-
-  // // load todos when App component renders
-  // useEffect( () => {
-  //   loadTodos()
-  // },[])
-
-  // // function handling loading todos
-  // function loadTodos() {
-  //   API.getTodos()
-  //   .then(res => {
-  //     setTodos(res.data);
-  //   })
-  //   .catch(err => console.log(err));
-  // }
   return (
     <AppProvider>
       <Home/>
