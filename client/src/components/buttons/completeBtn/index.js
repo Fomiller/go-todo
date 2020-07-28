5c46849bb8eef8c16ba5c89157e017c708a38412
Forrest <Forrestmillerj@gmail.com>
2020-07-28 17:01:32 -0500

    added update function to toggle completed status of todo

--- client/src/components/buttons/completeBtn/index.js
+++ client/src/components/buttons/completeBtn/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './completeBtn.css'
+import API from '../../../utils/api';
+import { useAppContext } from '../../../utils/globalContext';
 
-export default function CompleteBtn({props, onClick}){
+export default function CompleteBtn({props}){
+  const [state, dispatch] = useAppContext()
+
+  function handleUpdate(data){
+    API.updateTodo(data)
+    .then(res => {
+      loadTodos()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  function loadTodos() {
+    API.getTodos()
+    .then(res => {
+      dispatch({type: "SET_TODOS", payload:res.data});
+    })
+    .catch(err => console.log(err));
+  }
 
   return(
     <div>
-      <button data-todo-id={props.id} className="completeBtn" onClick={onClick}>Complete</button>
+      <button data-todo-id={props.id} data className="completeBtn" onClick={() => handleUpdate({id: props.id, completed: props.completed})}>Complete</button>
     </div>
   )
 }
