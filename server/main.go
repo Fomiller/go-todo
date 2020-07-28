e1e5cc856862b44e93b934df263227cbe058c7a6
Forrest <Forrestmillerj@gmail.com>
2020-07-28 16:58:14 -0500

    removed old code and comments

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	models "github.com/fomiller/go-todo/server/models/todos"
@@ -11,15 +10,11 @@ func main() {
 	// serve static files
 	fs := http.FileServer(http.Dir("../client/"))
 	http.Handle("/", fs)
+
 	// routes.
-	// http.HandleFunc("/", indexhandler)
-	http.HandleFunc("/api/todo/", models.Test)
+	http.HandleFunc("/api/todo/", models.TodoHandler)
 	// http.HandleFunc("/api/create/", models.CreateTodo)
 
 	// start server.
 	http.ListenAndServe(":8080", nil)
-}
-
-func indexhandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "welcome to go")
 }
