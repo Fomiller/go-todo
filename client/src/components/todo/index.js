4ee0d2e1102bc7c585f33a65a24e787347bfe2a2
Forrest <Forrestmillerj@gmail.com>
2020-07-29 17:18:20 -0500

    styling changes with bootstrap classes

--- client/src/components/todo/index.js
+++ client/src/components/todo/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 export default function Todo(props){
   return(
-    <div>
-      <h2>{props.info.todo}</h2>
-      <h2>{props.info.time}</h2>
-      {!props.info.completed ? (<h4>Completed: false</h4>) : (<h4>Completed: true</h4>)}
-      {props.children}
+    <div className="card my-3">
+      <div className="card-body">
+        <div className="row">
+          <div className="col">
+            <h2>{props.props.todo}</h2>
+            {!props.props.completed ? (<h4>Completed: false</h4>) : (<h4>Completed: true</h4>)}
+            <p>{props.props.time}</p>
+            {props.children}
+          </div>
+        </div>      
+      </div>
     </div>
   )
 }
